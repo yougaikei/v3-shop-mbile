d5fb4260b66225470f771f90988d9f882ffcb02f
苏常青 <2244915205>
2022-09-06 12:48:51 +0800

    axios 创建并初始化完成

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>Loading...</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/utils/request.js
@@ -0,0 +1,8 @@
+// axios
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'https://shop.fed.lagounews.com/api'
+})
+
+export default request
