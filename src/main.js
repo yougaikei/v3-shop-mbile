2c1192c85efe909ec2c6f8d3adfadd02d8de44c1
苏常青 <2244915205>
2022-09-06 21:02:36 +0800

    Vant 的引入

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// 引入 VueRouter 组件
 import router from './router'
 
+// 引入 vant 组件
+import Vant from 'vant'
+// 引入 vant 样式
+import 'vant/lib/index.css'
+
+// 引入适配样式
 import 'amfe-flexible'
 
 // 引入自己的公共样式
 import '@/styles/base.css'
 
+
 const app = createApp(App);
+// 引入 Router
 app.use(router);
+// 引入 vant
+app.use(Vant);
+// 挂载 app
 app.mount('#app');
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,6 +2,7 @@
 	<div>
 		<p>Home</p>
 		<p>首页页面</p>
+		<van-button>确认</van-button>
 	</div>
 </template>
 
